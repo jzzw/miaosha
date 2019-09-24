81385efc57f7e2f92c67234e1c2150ce95214019
zhongjiulin <jiuzhouzhiwai@163.com>
2019-09-24 20:56:12 +0800

    第一次提交

---
+++ src/main/java/com/zhbit/miaosha/controller/SampleController.java
@@ -0,0 +1,18 @@
+package com.zhbit.miaosha.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class SampleController {
+
+    @RequestMapping("/thymeleaf")
+    public String thymeleaf(Model model){
+        model.addAttribute("name","Joshu");
+        return "hello";
+    }
+
+
+}
